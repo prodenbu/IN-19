0a7bb557feafc96b62f0e1eaaa4e15cdee873522
prodenbu <61791103+prodenbu@users.noreply.github.com>
2020-06-02 15:00:37 +0200

    .

---
+++ Trimkowski/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    link:
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include 'vars.php';
+        echo $test;
+    ?>
+</body>
+</html>
